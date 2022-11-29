be44009ecb3ebaadc6c144c879b8a279028d3466
feliocha <ochandorenaf@gmail.com>
2022-11-29 19:46:29 -0300

    elimino funcion sin utilidad

--- app/models/articulos_model.php
+++ app/models/articulos_model.php
@@ -25,14 +25,6 @@ class ArticulosModel {
         $articulos = $query->fetchAll(PDO::FETCH_OBJ);
         return $articulos;
     }
-    
-    function getArticulosPorCat($id_categoria) {
-        $db = $this->getDB();
-        $query = $db->prepare("SELECT * FROM articulos WHERE id_categoria = ?");
-        $query->execute([$id_categoria]);
-        $articulos = $query->fetchAll(PDO::FETCH_OBJ);
-        return $articulos;
-    }
 
     function getArticulo($id) {
         $db = $this->getDB();
